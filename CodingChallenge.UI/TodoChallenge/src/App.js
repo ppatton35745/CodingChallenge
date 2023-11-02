af1be0c64a9fa41a55545e155a1deaf42bc5b6ad
Phil P <ppatton35745@gmail.com>
2023-11-01 21:00:13 -0500

    cleanup app js and rename todoadd.js

--- CodingChallenge.UI/TodoChallenge/src/App.js
+++ CodingChallenge.UI/TodoChallenge/src/App.js
@@ -1,43 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import TodoList from "./components/todo/TodoList";
-import {connect} from "react-redux";
+import TodoAdd from "./components/todo/TodoAdd";
 import "./App.scss";
 import './button.scss';
-import { addTodo } from './todoActions';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
 
-    this.state = {
-      newTodo: ''
-    };
-  }
-
-  textInputChange = (e) =>  {
-    this.setState({...this.state, newTodo: e.target.value});
-  }
-
-  addNewTodo = () => {
-    console.warn('not implemented');
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <input type="text" value={this.state.newTodo} onChange={this.textInputChange}></input>
-        <button className={"btn--default"} onClick={this.addNewTodo}>Add</button>
-        <TodoList />
-      </div>
-  )}
+  return (
+    <div className="App">
+      <TodoAdd />
+      <TodoList />
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => ({
-  todos: state.todos ?? []
-});
-const mapDispatchToProps = (dispatch) => ({
-  onAddTodo: (text) => dispatch(addTodo(text)),
-
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
